fe54a4f3caf4cea1d22851575d69a6140a76f8b8
Arnav-Gr0ver <arnavgrover@gmail.com>
2025-03-11 12:45:24 +0000

    update config + depenencies
    Signed-off-by: Arnav-Gr0ver <arnavgrover@gmail.com>

--- Task_2_FinRL_AlphaSeek_Crypto/data_config.py
+++ Task_2_FinRL_AlphaSeek_Crypto/data_config.py
@@ -2,4 +2,4 @@ class ConfigData:
     def __init__(self, data_dir: str = "./data"):
         self.data_dir = data_dir
         self.csv_path = f"{data_dir}/BTC_1sec.csv"
-        self.predict_ary_path = f"{data_dir}/BTC_1sec_predict.npy"
+        self.predict_ary_path = f"{data_dir}/BTC_1sec_input.npy"
